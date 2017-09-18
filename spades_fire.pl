5b3c49d6e77fcf6cdec72ba201bee4f1d082e614
Marc Chevrette <chevrm@gmail.com>
2017-09-18 15:42:20 -0500

    initial upload

---
+++ spades_fire.pl
@@ -0,0 +1,23 @@
+#!/bin/env perl
+
+use strict;
+use warnings;
+
+my $pipedir = '~/git/ilmn_spades';
+
+my %p = ();
+my @stage = glob("./src/*.fastq");
+foreach my $file (@stage){
+    my @p = split(/\// , $file);
+    my @u = split(/_+/, $p[-1]);
+    my $sid = $u[0];
+    #$sid = 'SID' . $sid;
+    if($file =~ m/R1/){
+	$p{$sid}{'left'} = $file;
+    }elsif($file =~ m/R2/){
+	$p{$sid}{'right'} = $file;
+    }
+}
+foreach my $s (keys %p){
+    system("perl $pipedir/spades_pipe.pl $p{$s}{'left'} $p{$s}{'right'} $s");
+}
